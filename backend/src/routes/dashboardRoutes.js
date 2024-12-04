d5a3f1d7fa0c2452ef90a6af4cc38bbb2749fe59
Maram <bmaram310@gmail.com>
2024-12-04 22:06:58 +0100

    commandes

--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { getAllCollectionsCsv } = require('../controllers/dashboardController');
+const { getCommandes } = require('../controllers/dashboardController');
 const router = express.Router();
 
 // Route pour récupérer les commandes
-router.get('/csv', getAllCollectionsCsv);
+router.get('/commandes', getCommandes);
 
 module.exports = router;
