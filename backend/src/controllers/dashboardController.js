b797e633894368bd51526626741b61fece33eb6d
Maram <bmaram310@gmail.com>
2024-12-05 12:42:04 +0100

    products

--- backend/src/controllers/dashboardController.js
+++ backend/src/controllers/dashboardController.js
@@ -14,8 +14,12 @@ const arrayToCsv = (data) => {
 // Fonction pour formater le champ `products`
 const formatProducts = (products) => {
   if (!Array.isArray(products)) return ''; // Si ce n'est pas un tableau, retourner une chaîne vide
-  return products.map(p => `${p.Product}${p.category}${p.price}${p.quantity}`).join('; '); // Exemple: "Product1 (x2); Product2 (x1)"
+  
+  return products
+    .map(p => `${p.id}`) // Séparer chaque champ par ";"
+    .join('; '); // Séparer chaque produit par "; "
 };
+
 
 // Contrôleur pour récupérer toutes les commandes et les envoyer au format CSV
 const getCommandes = async (req, res) => {
@@ -41,5 +45,32 @@ const getCommandes = async (req, res) => {
     res.status(500).send('Erreur lors de la récupération des commandes');
   }
 };
+// Contrôleur pour récupérer les produits et les envoyer au format CSV
+const getProducts = async (req, res) => {
+  try {
+    const productsRef = collection(firestore, 'products');
+    const snapshot = await getDocs(productsRef);
 
-module.exports = { getCommandes };
+    // Mapper les données des produits
+    const products = snapshot.docs.map(doc => ({
+      id: doc.id, // Inclure l'ID du document
+      Product: doc.data().Product || '', // Extraire le champ Product
+      category: doc.data().category || '', // Extraire le champ category
+      price: doc.data().price || '', // Extraire le champ price
+      quantity: doc.data().quantity || '', // Extraire le champ quantity
+      date: doc.data().createdAt|| '',
+    }));
+
+    // Transformer les données en CSV
+    const csvData = arrayToCsv(products);
+
+    // Envoyer les données CSV dans la réponse HTTP
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(csvData);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des produits :', error);
+    res.status(500).send('Erreur lors de la récupération des produits');
+  }
+};
+
+module.exports = { getCommandes ,getProducts };
--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { getCommandes } = require('../controllers/dashboardController');
+const { getCommandes, getProducts } = require('../controllers/dashboardController');
 const router = express.Router();
 
 // Route pour récupérer les commandes
 router.get('/commandes', getCommandes);
+
+// Route pour récupérer les produits
+router.get('/products', getProducts);
 
 module.exports = router;
